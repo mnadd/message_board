f9d5f6c598345565f1040326dd036bcc75e1293f
mio.nakagawa <mio_nkgw@yahoo.co.jp>
2022-03-07 14:53:46 +0900

    getAllMessagesを追加

--- src/models/Message.java
+++ src/models/Message.java
@@ -1,7 +1,5 @@
 package models;
 
-
-
 import java.sql.Timestamp;
 
 import javax.persistence.Column;
@@ -9,9 +7,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllMessages",
+            query = "SELECT m FROM Message AS m ORDER BY m.id DESC")
+})
 @Table(name = "messages")
 public class Message {
     @Id
